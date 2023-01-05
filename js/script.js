7747c736db5ac38347fcbc9cfe971486288489d9
zzagir <zaynalov005@mail.ru>
2023-01-05 19:57:20 +0300

    убрал бургер меню

--- js/script.js
+++
@@ -1,6 +0,0 @@
-let burger = document.querySelector('.menu-burger__header'),
-    menu = document.querySelector('.menu');
-
-burger.addEventListener('click', function(e) {
-  menu.classList.toggle('active');
-});
